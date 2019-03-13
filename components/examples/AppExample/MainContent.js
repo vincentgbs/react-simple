b5f2bfe4a429f00fb9963d1678d828234cf49a33
Vincent <vincent.gbs11@gmail.com>
2019-03-13 08:08:07 -0400

    Todo App - Phase 2

--- components/examples/AppExample/MainContent.js
+++ components/examples/AppExample/MainContent.js
@@ -1,6 +1,19 @@
 function MainContent() {
+    let date = new Date();
+
+    const toDoTitle = "Example App";
+    const headerStyle = {
+        // These properties can be dynamically changed with JS
+        color:"#09F",
+        backgroundColor:"#EEE"
+    }
+
     return (
         <main>
+            {/* //ES6 Syntax (string templating syntax) */}
+            {/* <h1>{`${toDoTitle}`}</h1> */}
+            <h1 style={headerStyle}>{toDoTitle}</h1>
+            <h2>It is currently about {date.getHours() % 12} o'clock!</h2>
             <ul>
                 <li>Thing 1</li>
                 <li>Thing 2</li>
--- components/examples/ToDoExample/App.js
+++ components/examples/ToDoExample/App.js
@@ -1,35 +1,15 @@
 /* If we were running an actual compiler for the JSX instead of an in-browser Babel transformer, we would use import and export instead of using html include tags */
 
 // import React from "react"
+// import TodoItem from "./TodoItem"
 
 function App() {
-    const toDoTitle = "To Do List";
-    let date = new Date();
-
-    const headerStyle = {
-        // These properties can be dynamically changed with JS
-        color:"#09F",
-        backgroundColor:"#EEE"
-    }
-
     return (
-        <div>
-            {/* //ES6 Syntax (string templating syntax) */}
-            {/* <h1>{`${toDoTitle}`}</h1> */}
-            <h1 style={headerStyle}>{toDoTitle}</h1>
-            <h2>It is currently about {date.getHours() % 12} o'clock!</h2>
-
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
-
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
-
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
-
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
+        <div className="todo-list">
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
         </div>
     );
 }
