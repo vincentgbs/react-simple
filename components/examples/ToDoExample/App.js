74ceefac3899940e4cc16361cbddf95e1102ee88
Vincent <vincent.gbs11@gmail.com>
2019-03-13 07:55:47 -0400

    Trying out inline styling in JSX

--- components/examples/ToDoExample/App.js
+++ components/examples/ToDoExample/App.js
@@ -3,8 +3,22 @@
 // import React from "react"
 
 function App() {
+    const toDoTitle = "To Do List";
+    let date = new Date();
+
+    const headerStyle = {
+        // These properties can be dynamically changed with JS
+        color:"#09F",
+        backgroundColor:"#EEE"
+    }
+
     return (
         <div>
+            {/* //ES6 Syntax (string templating syntax) */}
+            {/* <h1>{`${toDoTitle}`}</h1> */}
+            <h1 style={headerStyle}>{toDoTitle}</h1>
+            <h2>It is currently about {date.getHours() % 12} o'clock!</h2>
+
             <input type="checkbox" />
             <p>Placeholder text here</p>
 
