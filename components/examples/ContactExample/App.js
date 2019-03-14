0e72dd95e61f5067ecb1499c0cbc9bfa219f3617
Vincent <vincent.gbs11@gmail.com>
2019-03-13 22:24:51 -0400

    Moved contact into object (more likely to receive data in JSON format) instead of passing hard coded props

--- components/examples/ContactExample/App.js
+++ components/examples/ContactExample/App.js
@@ -5,32 +5,47 @@ function App() {
     return (
         <div className="contacts">
             <ContactCard
-                name="Mr. Whiskerson"
-                imgUrl="http://placekitten.com/300/200"
-                phone="(212) 555-1234"
-                email="mr.whiskaz@catnap.meow"
+                contact={
+                    {
+                        name: "Mr. Whiskerson",
+                        imgUrl: "http://placekitten.com/300/200",
+                        phone: "(212) 555-1234",
+                        email: "mr.whiskaz@catnap.meow"
+                    }
+                }
             />
-
             <ContactCard
-                name="Fluffykins"
-                imgUrl="http://placekitten.com/400/200"
-                phone="(212) 555-2345"
-                email="fluff@me.com"
+                contact={
+                    {
+                        name: "Fluffykins",
+                        imgUrl: "http://placekitten.com/400/200",
+                        phone: "(212) 555-2345",
+                        email: "fluff@me.com"
+                    }
+                }
             />
-
             <ContactCard
-                name="Destroyer"
-                imgUrl="http://placekitten.com/400/300"
-                phone="(212) 555-3456"
-                email="ofworlds@yahoo.com"
+                contact={
+                    {
+                        name: "Destroyer",
+                        imgUrl: "http://placekitten.com/400/300",
+                        phone: "(212) 555-3456",
+                        email: "ofworlds@yahoo.com"
+                    }
+                }
             />
-
             <ContactCard
-                name="Felix"
-                imgUrl="http://placekitten.com/200/100"
-                phone="(212) 555-4567"
-                email="thecat@hotmail.com"
+                contact={
+                    {
+                        name: "Felix",
+                        imgUrl: "http://placekitten.com/200/100",
+                        phone: "(212) 555-4567",
+                        email: "thecat@hotmail.com"
+                    }
+                }
             />
         </div>
     );
 }
+
+// export default App
