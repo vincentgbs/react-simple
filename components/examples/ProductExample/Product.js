c3697f3bf7568e43cb48015e907e498cdb0217fb
Vincent <vincent.gbs11@gmail.com>
2019-03-15 16:38:45 -0400

    basic mouse click event in react

--- components/examples/ProductExample/Product.js
+++ components/examples/ProductExample/Product.js
@@ -2,7 +2,7 @@ class Product extends React.Component {
     render() {
         return (
             <div>
-                <h2>{this.props.product.name}</h2>
+                <h2 onClick={() => {console.log("clicked")} }>{this.props.product.name}</h2>
                 <p>{this.props.product.price.toLocaleString("en-US", { style: "currency", currency: "USD" })} - {this.props.product.description}</p>
             </div>
         )
