763e330ea33408aa55915ae29044fc7fd9a6a6e2
Vincent <vincent.gbs11@gmail.com>
2019-03-16 12:24:46 -0400

    more conditional rendering

--- components/examples/ConditionalExample/Conditional.js
+++ components/examples/ConditionalExample/Conditional.js
@@ -1,22 +1,20 @@
 // import React from "react"
 
 function Conditional(props) {
-    return (
-        props.isLoading ? <h1>Loading...</h1> : <div>The API call has loaded and this is the new display</div>
-    );
-    // if(props.isLoading) {
-    //     return (
-    //         <h1>
-    //             Loading...
-    //         </h1>
-    //     );
-    // } else {
-    //     return (
-    //         <div>
-    //             The API call has loaded and this is the new display
-    //         </div>
-    //     );
-    // }
+    // return ( props.isLoading ? <h1>Loading</h1> : <div>Loaded</div> );
+    if(props.isLoading) {
+        return (
+            <h1>
+                Loading...
+            </h1>
+        );
+    } else {
+        return (
+            <div>
+                The API call has loaded and this is the new display
+            </div>
+        );
+    }
 }
 
 // export default Conditional
