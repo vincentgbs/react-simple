19834393bacb9c3677b015123a74d86a5c44399c
Vincent <vincent.gbs11@gmail.com>
2019-03-16 13:18:33 -0400

    Starting Forms Part 1

---
+++ components/examples/FormExample/App.js
@@ -0,0 +1,16 @@
+// https://reactjs.org/docs/forms.html
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+                Code goes here
+            </div>
+        );
+    }
+}
